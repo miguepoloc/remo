b69bd13eb864bc49111b355d846f3f9dba8bc7ca
miguepoloc <miguepoloc@gmail.com>
2021-01-21 11:37:07 -0500

    Añadiendo librerias

---
+++ apps/main/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from django.conf.urls import url
+from django.urls import path, include
+from rest_framework import routers
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    # url(r'^data/(?P<idx>\w+)/$', views.index2, name='index2'),
+]
