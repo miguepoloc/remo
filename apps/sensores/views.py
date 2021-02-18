5d7ae4e06c403643ec2aca8ba67533adc62c8196
miguepoloc <miguepoloc@gmail.com>
2021-02-18 12:17:38 -0500

    Historial de sensores ok

--- apps/sensores/views.py
+++ apps/sensores/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import generic
-from ..api.models import Sensor_Estacion
+from ..api.models import Sensor_Estacion, Sensor_Salidas_De_Campo
 
 
 class Sensor_EstacionListView(generic.ListView):
@@ -18,3 +18,11 @@ class Sensor_EstacionListView(generic.ListView):
 class Sensor_EstacionDetailView(generic.DetailView):
     model = Sensor_Estacion
     template_name = 'sensores/sensor_estacion_detail.html'
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        # Add in a QuerySet of all the books
+        context['salida_de_campo_list'] = Sensor_Salidas_De_Campo.objects.filter(
+            sensor=self.kwargs['pk'])
+        return context
