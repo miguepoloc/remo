ccf2c53f5836ff21b8ea6fbaf7f036b253bdcd4b
miguepoloc <miguepoloc@gmail.com>
2021-02-19 13:59:14 -0500

    Funciona agregar un nuevo componente, pero fallan los estilos

--- static/js/salidas_de_campo_form.js
+++ static/js/salidas_de_campo_form.js
@@ -42,30 +42,120 @@ $.get('/api/Sensor/', function (result) {
     console.log(lista_sensores);
 });
 
+var lista_tipo = "";
+
+$.get('/api/Tipo_Salida_De_Campo/', function (result) {
+    lista_tipo = result;
+    console.log(lista_tipo);
+});
+
+// Agregar un nuevo componente
+var cant_componentes = 0;
+var add_componente = document.getElementById('agregar_componente');
+$("#addComponente").click(function () {
+    let htmlx = "";
+    htmlx += '<div class="card card-primary ">';
+    htmlx += '<div class="card-header">';
+    htmlx += '<h3 class="card-title" id="nombre_componente' + cant_componentes + '"><i class="fas fa-tools"></i> Componente ' + cant_componentes + '</h3>';
+    htmlx += '</div>';
+    htmlx += '<div class="card-body">';
+    htmlx += '<div class="form-group">';
+    htmlx += '<label>Componente ' + cant_componentes + '</label>';
+    htmlx += '<select class="form-control select2" style="width: 100%;" id="select_componente' + cant_componentes + '">';
+    htmlx += '<option selected="selected" disabled="disabled">Selecciona un componente</option>';
+    htmlx += '</select>';
+    htmlx += '<label>Tipo de salida</label>';
+    htmlx += '<select class="form-control select2" style="width: 100%;" required id="select_componente_tipo' + cant_componentes + '">';
+    htmlx += '<option selected="selected" disabled="disabled">Selecciona el tipo de salida</option>';
+    for (let index = 0; index < lista_tipo.length; index++) {
+        htmlx += '<option value="' + lista_tipo[index].id + '">' + lista_tipo[index].tipo + '</option>';
+    }
+    htmlx += '</select>';
+    htmlx += '</div>';
+    htmlx += '<div class="form-group">';
+    htmlx += '<label>Observaciones</label>';
+    htmlx += '<textarea class="form-control" rows="3" cols="200" placeholder="Ingrese las actividades realizadas durante la salida de campo" id="observaciones_componente' + cant_componentes + '"></textarea>';
+    htmlx += '</div>';
+    htmlx += '</div>';
+    htmlx += '</div>';
+    htmlx += '</div>';
+    add_componente.insertAdjacentHTML('afterend', htmlx);
+    cant_componentes = cant_componentes + 1;
+});
+
+
+// Agregar un nuevo sensor
+var cant_sensores = 0;
+var add_sensor = document.getElementById('agregar_sensor');
+$("#addSensor").click(function () {
+    let htmlx = "";
+    htmlx += '<div class="card card-primary ">';
+    htmlx += '<div class="card-header">';
+    htmlx += '<h3 class="card-title" id="nombre_sensor' + cant_sensores + '"><i class="fas fa-cloud-sun-rain"></i> Sensor ' + cant_sensores + '</h3>';
+    htmlx += '</div>';
+    htmlx += '<div class="card-body">';
+    htmlx += '<div class="form-group">';
+    htmlx += '<label>Sensor ' + cant_sensores + '</label>';
+    htmlx += '<select class="form-control select2" style="width: 100%;" id="select_sensor' + cant_sensores + '">';
+    htmlx += '<option selected="selected" disabled="disabled">Selecciona un sensor</option>';
+    htmlx += '</select>';
+    htmlx += '<label>Tipo de salida</label>';
+    htmlx += '<select class="form-control select2" style="width: 100%;" required id="select_componente_tipo' + cant_sensores + '">';
+    htmlx += '<option selected="selected" disabled="disabled">Selecciona el tipo de salida</option>';
+    for (let index = 0; index < lista_tipo.length; index++) {
+        htmlx += '<option value="' + lista_tipo[index].id + '">' + lista_tipo[index].tipo + '</option>';
+    }
+    htmlx += '</select>';
+    htmlx += '</div>';
+    htmlx += '<div class="form-group">';
+    htmlx += '<label>Observaciones</label>';
+    htmlx += '<textarea class="form-control" rows="3" cols="200" placeholder="Ingrese las actividades realizadas durante la salida de campo" id="observaciones_sensor' + cant_sensores + '"></textarea>';
+    htmlx += '</div>';
+    htmlx += '</div>';
+    htmlx += '</div>';
+    htmlx += '</div>';
+    add_sensor.insertAdjacentHTML('afterend', htmlx);
+    cant_sensores = cant_sensores + 1;
+});
+
+
 // ----------- Select de estación -------------------
 var estacion_selecta = "";
 $('#select_estacion').on('select2:select', function (e) {
-    document.getElementById("select_componente").innerHTML = "<option selected='selected' disabled='disabled'>Selecciona un componente</option>";
-    document.getElementById('nombre_componente').innerHTML = '<i class="fas fa-tools"></i> Componente';
+    for (let index = 0; index < cant_sensores; index++) {
+        document.getElementById("select_sensor" + index).innerHTML = "<option selected='selected' disabled='disabled'>Selecciona un sensor</option>";
+        document.getElementById('nombre_sensor' + index).innerHTML = '<i class="fas fa-cloud-sun-rain"></i> Sensor';
+    }
+    for (let index = 0; index < cant_componentes; index++) {
+        document.getElementById("select_componente" + index).innerHTML = "<option selected='selected' disabled='disabled'>Selecciona un componente</option>";
+        document.getElementById('nombre_componente' + index).innerHTML = '<i class="fas fa-tools"></i> Componente';
+    }
     document.getElementById("select_sensor").innerHTML = "<option selected='selected' disabled='disabled'>Selecciona un sensor</option>";
     document.getElementById('nombre_sensor').innerHTML = '<i class="fas fa-cloud-sun-rain"></i> Sensor';
+    document.getElementById("select_componente").innerHTML = "<option selected='selected' disabled='disabled'>Selecciona un componente</option>";
+    document.getElementById('nombre_componente').innerHTML = '<i class="fas fa-tools"></i> Componente';
+
     var data_estacion = e.params.data;
     estacion_selecta = data_estacion.id;
     document.getElementById('nombre_estacion').innerHTML = '<i class="fas fa-broadcast-tower"></i> ' + data_estacion.text;
     // Se recorren todas las posiciones del vector que contiene a los componente
-    for (i = 0; i < lista_componentes_estacion.length; i++) {
-        if (lista_componentes_estacion[i].ubicacion == estacion_selecta) {
-            for (j = 0; j < lista_componentes.length; j++) {
-                if (lista_componentes[j].id == lista_componentes_estacion[i].componente) {
-                    // Se crea un elemento tipo option
-                    let z = document.createElement("option");
-                    // Al cual se le asigna el valor de la variable
-                    z.setAttribute("value", lista_componentes[j].id);
-                    // Y se le asigna al valor a mostrar
-                    let t = document.createTextNode(lista_componentes[j].nombre + " (" + lista_componentes[j].referencia + ")");
-                    z.appendChild(t);
-                    // Luego se añade al html
-                    document.getElementById("select_componente").appendChild(z);
+    for (let index = 0; index < cant_componentes; index++) {
+        for (i = 0; i < lista_componentes_estacion.length; i++) {
+            if (lista_componentes_estacion[i].ubicacion == estacion_selecta) {
+                for (j = 0; j < lista_componentes.length; j++) {
+                    if (lista_componentes[j].id == lista_componentes_estacion[i].componente) {
+                        // Se crea un elemento tipo option
+                        let z = document.createElement("option");
+                        // Al cual se le asigna el valor de la variable
+                        z.setAttribute("value", lista_componentes[j].id);
+                        // Y se le asigna al valor a mostrar
+                        let t = document.createTextNode(lista_componentes[j].nombre + " (" + lista_componentes[j].referencia + ")");
+                        z.appendChild(t);
+                        // Luego se añade al html
+                        document.getElementById("select_componente" + index).appendChild(z);
+                        console.log("select_componente" + index);
+                    }
+                    // document.getElementById("select_componente").appendChild(z);
                 }
             }
         }
@@ -190,18 +280,3 @@ function convertir_fecha(fecha) {
     dia = fecha.slice(8, 10);
     return (nueva_fecha = anio + "-" + mes + "-" + dia);
 }
-
-
-
-// var d1 = document.getElementById('Prueba');
-// $("#addSensor").click(function () {
-//     let htmlx = "";
-//     htmlx += '<div id="vidas" class="row">';
-//     htmlx += '<button type="button" class="btn btn-dark swalOK"><i class="fas fa-plus-circle"></i> Agregar sensor</button>';
-//     htmlx += '</div>';
-
-//     // document.getElementById("Prueba").innerHTML = htmlx;
-//     d1.insertAdjacentHTML('afterend', htmlx);
-
-//     // $("#Prueba").innerHTML(htmlx);
-// });
